4932c71d9a13d9a7dd06614721e8b1620b81f0de
Bianca Bispo <biancabss12@gmail.com>
2022-08-02 22:15:32 -0300

    Estrutura Condicional

---
+++ uri1001/src/uri/uri1009.java
@@ -0,0 +1,23 @@
+package uri;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class uri1009 {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        String nome = sc.next();
+        double salarioFixo = sc.nextDouble();
+        double TotalVendas = sc.nextDouble();
+        
+        double SalarioMes;
+        
+        SalarioMes = salarioFixo + (TotalVendas * 0.15);
+
+        System.out.printf("TOTAL = R$ %.2f%n", SalarioMes);
+        
+        sc.close();
+    }
+}
---
+++ uri1001/src/uri/uri1010.java
@@ -0,0 +1,12 @@
+package uri;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class uri1010 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        sc.close();
+    }
+}
